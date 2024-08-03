08aafa86cfa1c6d03cbf1b6d03bac5d3b94b38ce
wenjing-xin <wb3368316006@gmail.com>
2024-08-03 22:15:34 +0800

    更改配置获取方式

--- src/main/java/run/halo/dataAnalysis/matomo/MatomoTrackerWebFilter.java
+++ src/main/java/run/halo/dataAnalysis/matomo/MatomoTrackerWebFilter.java
@@ -7,12 +7,14 @@ import org.matomo.java.tracking.MatomoRequests;
 import org.matomo.java.tracking.MatomoTracker;
 import org.matomo.java.tracking.TrackerConfiguration;
 import org.matomo.java.tracking.parameters.VisitorId;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilterChain;
 import reactor.core.publisher.Mono;
 import run.halo.app.infra.ExternalUrlSupplier;
+import run.halo.app.plugin.ReactiveSettingFetcher;
 import run.halo.app.security.AdditionalWebFilter;
 import run.halo.dataAnalysis.entity.PluginSettings;
 import run.halo.dataAnalysis.service.PluginSettingsGetter;
@@ -33,31 +35,34 @@ public class MatomoTrackerWebFilter implements AdditionalWebFilter {
 
     private final ExternalUrlSupplier externalUrlSupplier;
 
-    private final PluginSettingsGetter pluginSettingsGetter;
+    private final ReactiveSettingFetcher settingFetcher;
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
 
-        ServerHttpRequest haloRequest = exchange.getRequest();
+        return settingFetcher.fetch(PluginSettings.BaseSettings.GROUP, PluginSettings.BaseSettings.class).flatMap(baseSettings -> {
 
-        TrackerConfiguration trackerConfiguration = initConfiguration();
-        MatomoTracker matomoTracker = new MatomoTracker(trackerConfiguration);
-        String uuidForVisitorId = UUIDGeneratorUtil.generateUUIDFromRequest(haloRequest);
-        VisitorId visitorIdFromUUID = VisitorId.fromUUID(UUID.fromString(uuidForVisitorId));
-        //创建单个跟踪请求数据类
-        MatomoRequest singleMatomoRequest = MatomoRequests
-            // 为内容展示创建 MatomoRequest 对象 name 参数后续要生成页面名称，文章名称 以及瞬间的ID等
-            .contentImpression("name", null, haloRequest.getURI().toString())
-            .eventCategory("request")
-            .eventAction("api-operation")
-            .visitorId(visitorIdFromUUID)
-            .build();
-        // 发送请求
-        matomoTracker.sendBulkRequestAsync(singleMatomoRequest);
+            ServerHttpRequest haloRequest = exchange.getRequest();
 
-        // 放行其他过滤器
-        chain.filter(exchange);
-        return Mono.empty();
+            TrackerConfiguration trackerConfiguration = initConfiguration(baseSettings);
+            MatomoTracker matomoTracker = new MatomoTracker(trackerConfiguration);
+            String uuidForVisitorId = UUIDGeneratorUtil.generateUUIDFromRequest(haloRequest);
+            VisitorId visitorIdFromUUID = VisitorId.fromUUID(UUID.fromString(uuidForVisitorId));
+            //创建单个跟踪请求数据类
+            MatomoRequest singleMatomoRequest = MatomoRequests
+                // 为内容展示创建 MatomoRequest 对象 name 参数后续要生成页面名称，文章名称 以及瞬间的ID等
+                .contentImpression("name", null, haloRequest.getURI().toString())
+                .eventCategory("request")
+                .eventAction("api-operation")
+                .visitorId(visitorIdFromUUID)
+                .build();
+            // 发送请求
+            matomoTracker.sendBulkRequestAsync(singleMatomoRequest);
+
+            // 放行其他过滤器
+            chain.filter(exchange);
+            return Mono.empty();
+        });
     }
 
     /**
@@ -67,11 +72,10 @@ public class MatomoTrackerWebFilter implements AdditionalWebFilter {
      * 默认数据包之间的最大不活动时间为5s
      * @return
      */
-    public TrackerConfiguration initConfiguration(){
-
-        PluginSettings.BaseSettings basicSettings = pluginSettingsGetter.getBasicSettings().blockOptional().orElseThrow();
+    public TrackerConfiguration initConfiguration(PluginSettings.BaseSettings basicSettings){
 
         return TrackerConfiguration.builder()
+            .userAgent("org.matomo.java.tracking.Java11SenderProvider")
             .apiEndpoint(URI.create(externalUrlSupplier.getRaw().toString()))
             .connectTimeout(Duration.ofSeconds(3))
             .defaultAuthToken(basicSettings.getDefaultToken())
@@ -81,6 +85,5 @@ public class MatomoTrackerWebFilter implements AdditionalWebFilter {
             .defaultSiteId(1)
             .build();
     }
-
 
 }
