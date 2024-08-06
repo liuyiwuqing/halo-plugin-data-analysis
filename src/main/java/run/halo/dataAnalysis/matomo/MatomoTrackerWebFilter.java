cddb124a0b6a463d6bfcd70e9d11b8cafc7f11bc
wenjing-xin <wb3368316006@gmail.com>
2024-08-06 12:37:07 +0800

    基础埋点demo

--- src/main/java/run/halo/dataAnalysis/matomo/MatomoTrackerWebFilter.java
+++ src/main/java/run/halo/dataAnalysis/matomo/MatomoTrackerWebFilter.java
@@ -7,7 +7,6 @@ import org.matomo.java.tracking.MatomoRequests;
 import org.matomo.java.tracking.MatomoTracker;
 import org.matomo.java.tracking.TrackerConfiguration;
 import org.matomo.java.tracking.parameters.VisitorId;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
@@ -17,12 +16,11 @@ import run.halo.app.infra.ExternalUrlSupplier;
 import run.halo.app.plugin.ReactiveSettingFetcher;
 import run.halo.app.security.AdditionalWebFilter;
 import run.halo.dataAnalysis.entity.PluginSettings;
-import run.halo.dataAnalysis.service.PluginSettingsGetter;
 import run.halo.dataAnalysis.util.UUIDGeneratorUtil;
 import java.net.URI;
 import java.time.Duration;
 import java.util.UUID;
-
+import org.matomo.java.tracking.Java11SenderProvider;
 /**
  * 在相应的请求中注入跟踪器
  * @author: dreamChaser
@@ -46,19 +44,18 @@ public class MatomoTrackerWebFilter implements AdditionalWebFilter {
 
             TrackerConfiguration trackerConfiguration = initConfiguration(baseSettings);
             MatomoTracker matomoTracker = new MatomoTracker(trackerConfiguration);
+
             String uuidForVisitorId = UUIDGeneratorUtil.generateUUIDFromRequest(haloRequest);
             VisitorId visitorIdFromUUID = VisitorId.fromUUID(UUID.fromString(uuidForVisitorId));
+
             //创建单个跟踪请求数据类
             MatomoRequest singleMatomoRequest = MatomoRequests
                 // 为内容展示创建 MatomoRequest 对象 name 参数后续要生成页面名称，文章名称 以及瞬间的ID等
                 .contentImpression("name", null, haloRequest.getURI().toString())
-                .eventCategory("request")
-                .eventAction("api-operation")
                 .visitorId(visitorIdFromUUID)
                 .build();
             // 发送请求
             matomoTracker.sendBulkRequestAsync(singleMatomoRequest);
-
             // 放行其他过滤器
             chain.filter(exchange);
             return Mono.empty();
@@ -73,9 +70,7 @@ public class MatomoTrackerWebFilter implements AdditionalWebFilter {
      * @return
      */
     public TrackerConfiguration initConfiguration(PluginSettings.BaseSettings basicSettings){
-
         return TrackerConfiguration.builder()
-            .userAgent("org.matomo.java.tracking.Java11SenderProvider")
             .apiEndpoint(URI.create(externalUrlSupplier.getRaw().toString()))
             .connectTimeout(Duration.ofSeconds(3))
             .defaultAuthToken(basicSettings.getDefaultToken())
